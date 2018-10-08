d68bfb9cb2f05586a0020ead9da5c7c5e1128b3a
Vincent Letourneau <vincent@nanoninja.com>
2018-10-08 17:21:28 +0200

    j1 end

--- static/js/app.js
+++ static/js/app.js
@@ -2,19 +2,24 @@
 const sectionPlayers = document.getElementById('players');
 const selectTeam = document.getElementById('selectTeam');
 const resultNumber = document.getElementById('resultNumber');
+const cbActive = document.getElementById('cbActive');
 
 // Source de données globales (accessibles à toutes les fonctions)
 let players = [
-  {photoUrl:'https://medias.lequipe.fr/img-photo-jpg/09-12-17-torino-allianz-stadium-serie-a-16a-giornata-juventus-inter-nella-foto-blaise-matuidi/1500000000891906/121:245,2417:1775-624-416-75/67f39.jpg', firstname: 'Blaise', lastname: 'Matuidi', num: 16, team: 'Juventus'},
-  {photoUrl:'static/img/bonucci.jpeg', firstname: 'Paolo', lastname: 'Dybala', num: 10, team: 'Juventus'},
-  {photoUrl:'static/img/dybala.jpg', firstname: 'Leo', lastname: 'Bonucci', num: 19, team: 'Juventus'},
-  {photoUrl:'', firstname: 'Kylian', lastname: 'Mbappé', num: 7, team: 'PSG'}
+  {active: true, photoUrl:'https://medias.lequipe.fr/img-photo-jpg/09-12-17-torino-allianz-stadium-serie-a-16a-giornata-juventus-inter-nella-foto-blaise-matuidi/1500000000891906/121:245,2417:1775-624-416-75/67f39.jpg', firstname: 'Blaise', lastname: 'Matuidi', num: 16, team: 'Juventus'},
+  {active: false, photoUrl:'static/img/bonucci.jpeg', firstname: 'Paolo', lastname: 'Dybala', num: 10, team: 'Juventus'},
+  {active: false, photoUrl:'static/img/dybala.jpg', firstname: 'Leo', lastname: 'Bonucci', num: 19, team: 'Juventus'},
+  {active: true, photoUrl:'', firstname: 'Kylian', lastname: 'Mbappé', num: 7, team: 'PSG'}
 ];
 
 // Déclaration d'événements
 selectTeam.addEventListener('change', e => {
   //console.log(e.target.value);
-  buildTable(e.target.value);
+  buildTable(e.target.value, false);
+})
+
+cbActive.addEventListener('click', e => {
+  buildTable(null, e.target.checked);
 })
 
 // Fonctions
@@ -22,11 +27,11 @@ function test() {
   console.log('test');
 }
 
-function buildTable(selectedTeam) {
+function buildTable(selectedTeam, activePlayers) {
   //console.log(selectedTeam);
   let nbVisiblePlayers = players.length;
   let list = '<table class="table table-bordered table-striped">';
-  list += '<thead><tr><th>Photo</th><th>Nom</th><th>Prénom</th><th>Equipe</th></tr></thead>';
+  list += '<thead><tr><th>Photo</th><th>Nom</th><th>Prénom</th><th>Equipe</th><th>Numéro</th</tr></thead>';
 
   // ES6 (arrow function)
   players.forEach(player => {
@@ -39,7 +44,12 @@ function buildTable(selectedTeam) {
         list += '<tr>';
       }
     } else {
-      list += '<tr>';
+      if (player.active != activePlayers && activePlayers) {
+        list += '<tr style="display:none">';
+        nbVisiblePlayers--; // décrémentation
+      } else {
+        list += '<tr>';
+      }
     }
 
     // if (player.lastname.indexOf('ci') != -1) {
@@ -57,6 +67,7 @@ function buildTable(selectedTeam) {
     list += '<td>' + player.lastname + '</td>';
     list += '<td>' + player.firstname + '</td>';
     list += '<td>' + player.team + '</td>';
+    list += '<td><div class="playerNumber">' + player.num + '</span></td>';
     list += '</tr>';
   })
   list += '</table>';
@@ -65,7 +76,7 @@ function buildTable(selectedTeam) {
 }
 
 function init() {
-  buildTable(null);
+  buildTable(null, false);
 }
 
 // Initialisation
